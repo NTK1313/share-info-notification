da4fb812f67d681353df15509e30442037d3ce72
NTK <38352784+NTK1313@users.noreply.github.com>
2023-03-07 12:39:28 +0900

    sqlファイル参照先変更

--- src/javascript/express-api/route/execSql.js
+++ src/javascript/express-api/route/execSql.js
@@ -1,5 +1,5 @@
 exports.sqlReader = function (sqlFile) {
-	var file = "C:/Users/nnsks/github_local/line_notification/src/sql/" + sqlFile;
+	var file = "C:/Users/nnsks/github_local/share_info_notification/src/sql/" + sqlFile;
 	var fs = require("fs");
 	const data = fs.readFileSync(file, "utf-8");
 	console.log(data);
