047759bc08c79b7bcaffa7fae7c9be0189869ea9
NTK <38352784+NTK1313@users.noreply.github.com>
2023-03-14 16:57:53 +0900

    YahooファイナンスAPIで株価取得

--- src/javascript/shareInfo.js
+++ src/javascript/shareInfo.js
@@ -1,9 +1,21 @@
 callApi();
 async function callApi() {
 	const res = await fetch("http://127.0.0.1:3000/api/v1/article1/getDBData");
+	/** JSONイメージ
+		[
+			{
+				"銘柄コード": 9432,
+				"企業名": "NTT",
+				"市場区分": "プライム",
+				"株価": null,
+				"前日からの変動値": null
+			}
+		]
+	*/
 	const result = await res.json();
 	// DBから返却された結果を画面表示する。
 	const str = JSON.stringify(result);
+	// 最新の株価取得用のボタンは分けるか。
 	const res2 = await fetch("http://127.0.0.1:3000/api/v1/article1/getPrice", {
 		method: 'POST',
 		headers: {
@@ -11,31 +23,33 @@ async function callApi() {
 		},
 		body: str
 	});
-	const result2 = await res2.json();
-	console.log('POST');
-	console.log(result2);
-
 	/** JSONイメージ
-	[
-		{
-			"br_cd": 3092,
-			"br_name": "ZOZO",
-			"market_segment": "プライム"
-		},
-		{
-			"br_cd": 4220,
-			"br_name": "リケンテクノス",
-			"market_segment": "プライム"
+		[
+			{
+				"銘柄コード": 9432,
+				"株価": 3964,
+				"前日からの変動値": -22
+			}
+		]
+	*/
+	const result2 = await res2.json();
+	console.log(result2);
+	// strの銘柄コードをKeyとして株価と変動値をaddする
+	for (let i = 0; i < result.length; i++) {
+		var code = result[i]['銘柄コード'];
+		for (let j = 0; j < result2.length; j++) {
+			if (code == result2[j]['銘柄コード']) {
+				result[i]['株価'] = result2[j]['株価'];
+				result[i]['前日からの変動値'] = result2[j]['前日からの変動値'];
+			}
 		}
-	]
-*/
+	}
+
 	//  表作成
 	var table = document.getElementById("table");
-	// JSONのparse
-	const obj = JSON.parse(str);
 	var data = '';
-	for (let i = 0; i < obj.length; i++) {
-		const share = obj[i];
+	for (let i = 0; i < result.length; i++) {
+		const share = result[i];
 		const keyList = Object.keys(share);
 
 		// ヘッダ作成
