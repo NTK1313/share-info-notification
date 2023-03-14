f7d6a8c6beb4d0300a09329ed4402ce09b35dc75
NTK <38352784+NTK1313@users.noreply.github.com>
2023-03-14 17:01:55 +0900

    最新化

--- DDL/テーブル生成用DDLテンプレート.sql
+++ DDL/テーブル生成用DDLテンプレート.sql
@@ -1,6 +1,13 @@
 -- テーブル名変更
 ALTER TABLE m_stock rename TO m_stock_jp
 
+-- カラム名変更
+alter table t_stock_jp rename  stock_price to regular_market_price
+
+-- カラム追加
+ALTER TABLE t_stock_jp ADD COLUMN regular_market_change character varying(10)
+
+
 -- テーブル生成用DDLテンプレート
 -- 株価マスタ（日本）
 CREATE TABLE public.m_stock_jp (
@@ -36,4 +43,17 @@ COMMENT ON COLUMN m_stock_en.regis_date IS '登録日時';
 COMMENT ON COLUMN m_stock_en.update_date IS '更新日時';
 COMMENT ON COLUMN m_stock_en.delflg IS '削除フラグ';
 
-
+-- 株価トラン（日本）
+CREATE TABLE public.t_stock_jp (
+    br_cd integer NOT NULL,
+    shori_date timestamp without time zone NOT NULL,
+    regis_date timestamp without time zone NOT NULL,
+    regular_market_price character varying(10),
+    regular_market_change character varying(10),
+	primary key(br_cd,shori_date)
+);
+COMMENT ON COLUMN t_stock_jp.br_cd IS '銘柄コード';
+COMMENT ON COLUMN t_stock_jp.shori_date IS '処理日時';
+COMMENT ON COLUMN t_stock_jp.regis_date IS '登録日時';
+COMMENT ON COLUMN t_stock_jp.regular_market_price IS '株価';
+COMMENT ON COLUMN t_stock_jp.regular_market_change IS '前日からの変動値';
