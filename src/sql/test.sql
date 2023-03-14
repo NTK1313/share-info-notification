cf86c21b984c6ef63c16ddbba265567378286a64
NTK <38352784+NTK1313@users.noreply.github.com>
2023-03-14 17:01:20 +0900

    最新化

--- src/sql/test.sql
+++ src/sql/test.sql
@@ -1,7 +1,9 @@
 select
-	a.br_cd
-,	a.br_name
-,	a.market_segment
+	a.br_cd as "銘柄コード",
+	a.br_name as "企業名",
+	a.market_segment as "市場区分",
+	b.regular_market_price as "株価",
+	b.regular_market_change as "前日からの変動値"
 from
-	m_stock_jp	a
-;
+	m_stock_jp a
+	left join t_stock_jp b on a.br_cd = b.br_cd
